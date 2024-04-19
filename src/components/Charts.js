9ce31d9c57f99cf248217ab71892ab95e0d07738
wmcgloin <wtm30@georgetown.edu>
2024-04-18 20:14:26 -0400

    chord genre chart

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -2,6 +2,7 @@
 
 import AreaChartPlot from "./AreaChartPlot";
 import AreaChartHero from "./test";
+import ChordDiagram from "././GenreInteractionChart";
 
 const Charts = () => {
   return (
@@ -48,7 +49,7 @@ const Charts = () => {
       <section className="my-4 px-4">
         <div className="flex gap-3 mb-2 justify-center">
             <div className="w-1/2 flex justify-center item-center"> <p className="text-gray-900 font-bold">Titles Released Over Time</p></div>
-            <div className="w-1/2 flex justify-center item-center"> <p className="text-gray-900 font-bold">Revenue Over Time</p></div>
+            <div className="w-1/2 flex justify-center item-center"> <p className="text-gray-900 font-bold">Genre Chord</p></div>
         </div>
         <div className="flex gap-3">
             <div className="w-1/2 h-[300px] bg-gray-700 rounded flex justify-center items-center p-2">
@@ -56,7 +57,7 @@ const Charts = () => {
             </div>
             
             <div className="w-1/2 h-[300px] bg-gray-700 rounded flex justify-center items-center p-2">
-              <AreaChartHero />
+                <ChordDiagram />
             </div>
         </div>
       </section>
