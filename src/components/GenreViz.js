35f9fbf957754665356f32f41c940fc99487e044
Isfar Baset <iib2@georgetown.edu>
2024-04-26 13:17:01 -0400

    placeholder chart added to dashboard

---
+++ src/components/GenreViz.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import data from '../../public/genre_data.json';  // Path to JSON file
+
+export default class GenreTrendsChart extends PureComponent {
+  render() {
+    return (
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 20,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="year" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Action" fill="#8884d8" />
+          <Bar dataKey="Adventure" fill="#82ca9d" />
+          <Bar dataKey="Comedy" fill="#ffc658" />
+          // Add more bars for each genre as needed
+        </BarChart>
+      </ResponsiveContainer>
+    );
+  }
+}
