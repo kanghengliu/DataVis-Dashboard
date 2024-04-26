5a8ce7c5f27a06f067b3c4da8b56d9067b1f7d54
wmcgloin <wtm30@georgetown.edu>
2024-04-25 22:24:09 -0400

    best map marker ever

--- src/components/titles/map.js
+++ src/components/titles/map.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+// Assuming your image is in the public directory and accessible at the '/HERE!.png' path
+const customIcon = L.icon({
+    iconUrl: '/HERE!.png',
+    iconSize: [37, 37],    // Size of the icon, adjust as necessary
+    iconAnchor: [25, 50],  // Point of the icon which will correspond to marker's location
+    popupAnchor: [0, -50], // Point from which the popup should open relative to the iconAnchor
+});
 
 const MoviesMap = ({ tconst }) => {
     const [locations, setLocations] = useState([]);
 
-    // Load the locations data
     useEffect(() => {
         fetch('/data/movie_locations.json')
             .then(res => res.json())
@@ -18,11 +26,13 @@ const MoviesMap = ({ tconst }) => {
 
     return (
         <MapContainer center={[20, 0]} zoom={2} style={{ height: '500px', width: '100%' }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             {locations.map((loc, index) => (
-                <Marker key={index} position={[loc.latitude, loc.longitude]}>
+                <Marker
+                    key={`${loc.tconst}-${index}`}
+                    position={[loc.latitude, loc.longitude]}
+                    icon={customIcon}  // Use the custom icon here
+                >
                     <Tooltip>{loc.location}</Tooltip>
                 </Marker>
             ))}
